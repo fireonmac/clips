6ec24199978661af7570447a0b5fdacdf51fb5c0
Paul Noh <fireonmac@gmail.com>
2022-04-21 22:27:07 +0900

    add register form

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  age = new FormControl('', [
+    Validators.min(18),
+    Validators.max(120),
+  ]); 
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  confirmPassword = new FormControl('', [
+    Validators.required
+  ]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13),
+  ]);
+  
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age, 
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    
+  }
+}
